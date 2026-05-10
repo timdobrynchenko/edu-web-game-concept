d4767b0d0bfb5f0b6366298e0d6ff1f410917d23
Tim Dobrynchenko <126889400+timdobrynchenko@users.noreply.github.com>
2026-05-10 16:38:36 +0300

    Installed and configured npm, Babel and ESlint

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+/**
+ * THIS FILE WAS AUTO-GENERATED.
+ * PLEASE DO NOT EDIT IT MANUALLY.
+ * ===============================
+ * IF YOU COPY THIS INTO AN ESLINT CONFIG, REMOVE THIS COMMENT BLOCK.
+ */
+
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
+
+const gitignorePath = path.resolve('.', '.gitignore');
+
+const jsConfig = defineConfig([
+  // ESLint recommended config
+  {
+    name: 'js/config',
+    ...js.configs.recommended,
+  },
+  // Stylistic plugin
+  plugins.stylistic,
+  // Import X plugin
+  plugins.importX,
+  // Airbnb base recommended config
+  ...configs.base.recommended,
+]);
+
+const nodeConfig = defineConfig([
+  // Node plugin
+  plugins.node,
+  // Airbnb Node recommended config
+  ...configs.node.recommended,
+]);
+
+export default defineConfig([
+  // Ignore files and folders listed in .gitignore
+  includeIgnoreFile(gitignorePath),
+  // JavaScript config
+  ...jsConfig,
+  // Node config
+  ...nodeConfig,
+]);
